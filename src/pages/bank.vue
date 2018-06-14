de343a926651a3c5ffd86de8ab1aa5fff09e9b78
Murilo Angelo <muriloangelo10@gmail.com>
2018-06-14 14:12:47 -0300

    correção de erro

--- src/pages/bank.vue
+++ src/pages/bank.vue
@@ -118,44 +118,6 @@
           </div>
       </div>
     </div>
-    <q-modal v-model="opened" :content-css="{minWidth: '80vw', minHeight: '80vh'}">
-  <q-modal-layout>
-    <q-toolbar slot="header">
-      <q-btn
-        flat
-        round
-        dense
-        v-close-overlay
-        icon="keyboard_arrow_left"
-      />
-      <q-toolbar-title>
-        Header
-      </q-toolbar-title>
-    </q-toolbar>
-
-    <q-toolbar slot="header">
-      <q-search inverted v-model="" color="none" />
-    </q-toolbar>
-
-    <q-toolbar slot="footer">
-      <q-toolbar-title>
-        Footer
-      </q-toolbar-title>
-    </q-toolbar>
-
-    <div class="layout-padding">
-      <h1>Modal</h1>
-
-      <q-btn
-        color="primary"
-        v-close-overlay
-        label="Close"
-      />
-
-      <p>This is a Modal presenting a Layout.</p>
-    </div>
-  </q-modal-layout>
-</q-modal>
   </div>
 </template>
 
